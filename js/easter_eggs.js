0ed975ed545bc27ef156e4267dde90e864db0a04
Boxuan Cui <boxuancui@gmail.com>
2015-04-28 21:37:36 -0400

    finished general skeleton

---
+++ js/easter_eggs.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#bo_fool_around_1").mouseover(function(){$(this).text("I'm kidding, now go back to work! Ciao~").delay(1200).fadeOut('slow');})
+    $("#bo_fool_around_2").mouseover(function(){$(this).text("There is something called Email if you miss him.").delay(1200).fadeOut('slow');})
+    $("#bo_fool_around_3").click(function(){$(this).fadeOut('slow');})
+})
